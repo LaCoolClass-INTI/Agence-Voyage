48464ab5b0c73c14a015944bfdd8459e5e6b7008
Léon Guichard <l.guichard.95@gmail.com>
2022-02-16 14:36:19 +0100

    Ajout creation des tableaux

---
+++ Leon-Guillaume/Create.sql
@@ -0,0 +1,32 @@
+Create table STATION (nomStation varchar(20), 
+						capacite int not null, 
+						lieu varchar(20) not null, 
+						region varchar(20), 
+						tarif int Default 0, 
+						CONSTRAINT cle_station PRIMARY KEY (nomStation), 
+						CONSTRAINT cle_lieu_region UNIQUE (lieu, region), 
+						CONSTRAINT nom_region CHECK (region IN ('OCEAN INDIEN', 'ANTILLES', 'EUROPE', 'AMERIQUES', 'EXTREME ORIENT'))
+						); 
+
+Create table ACTIVITE (nomStation varchar(20), 
+						libelle varchar(20), 
+						prix int Default 0, 
+						PRIMARY KEY (nomStation, libelle), 
+						);
+
+Create table CLIENT (ID int, 
+						nom varchar(20) not null, 
+						prenom varchar(20), 
+						ville varchar(20) not null, 
+						région varchar(20), 
+						solde int Default 0 not null, 
+						PRIMARY KEY (ID)
+						);
+
+Create table SEJOUR (ID int, 
+						station varchar(20), 
+						debut Date, 
+						nbPlaces int not null, 
+						PRIMARY KEY (ID, station, debut), 
+						FOREIGN KEY (ID) REFERENCES CLIENT, 
+						);
